ec842a441b5e7f22ca1df3eeb6bdd38b2efdd70b
MatinChen <matin_chen@126.com>
2026-07-02 13:17:01 +0800

    状态机完善，加游戏主循环初始化

---
+++ Service/core/GameLoop.js
@@ -0,0 +1,15 @@
+//游戏主循环（生命周期）
+function GameLoop() {
+     switch(currentState) {
+        case GameState.START:
+            break;
+        case GameState.PLAYING:
+            break;
+        case GameState.PAUSED:
+            break;
+        case GameState.WIN:
+            break;
+        case GameState.LOSE:
+            break;
+     }
+}
--- Service/core/State Machine.js
+++ Service/core/State Machine.js
@@ -1,8 +1,16 @@
 //状态机
 const GameState = {
-    START: "Start",
-    PLAYING: "Playing",
+    START: "Start",    // 开始界面
+    PLAYING: "Playing",// 进行中状态
     PAUSED: "Paused",
     WIN: "WIN",
     LOSE: "LOSE"
+}
+
+//当前状态
+let currentState;
+
+//状态转换函数
+function setState(newState){
+    currentState = newState;
 }
